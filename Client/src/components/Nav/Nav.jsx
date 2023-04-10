3d96bce1ddf15c89e5f3cbabd0bdcc8a14b5fa16
Duvan <duvanserrano21@gmail.com>
2023-04-10 11:13:37 -0500

    Adelanto

---
+++ Client/src/components/Nav/Nav.jsx
@@ -0,0 +1,61 @@
+import style from './Nav.module.css'
+import { NavLink, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faRightFromBracket, faCircleUser, faHeart, faBars } from '@fortawesome/free-solid-svg-icons'
+import logo from '../img/logo.png'
+import { useState } from 'react'
+
+ 
+
+ const Nav = ({ logout }) => {
+
+   const [menuHamburguesa, setMenuHamburguesa] = useState(false)
+
+   const location = useLocation();
+
+  // Si la ruta actual es '/', no mostramos la barra de navegación
+  if (location.pathname === "/") {
+    return null;
+  }
+
+   return (
+      <>
+     <nav className={style.nav}>
+         <div className={style.logo}><img src={logo} alt=''/></div>
+            {menuHamburguesa ? 
+            <div className={style.items}>
+               <NavLink className={style.linkMenu} to='/home'>
+                  <div className={style.itemsDiv} onClick={() => setMenuHamburguesa(!menuHamburguesa)}><div> <FontAwesomeIcon icon={faHouse}/>  Home</div></div>
+               </NavLink> 
+               <NavLink className={style.linkMenu} to='/favorites'>
+                  <div className={style.itemsDiv} onClick={() => setMenuHamburguesa(!menuHamburguesa)}><div><FontAwesomeIcon icon={faHeart} />  Favorites</div></div>
+               </NavLink>
+               <NavLink className={style.linkMenu} to='/about'>
+                  <div className={style.itemsDiv} onClick={() => setMenuHamburguesa(!menuHamburguesa)}><div><FontAwesomeIcon icon={faCircleUser} />  About Me</div></div>
+               </NavLink>
+               <div onClick={logout} className={style.itemsDiv}><div><FontAwesomeIcon icon={faRightFromBracket} />  Log Out</div></div>
+            </div> : 
+            <div className={style.item}>
+               <NavLink className={style.linkMenu} >
+                  <div className={style.itemsDiv} ><div className={style.ocltar}><FontAwesomeIcon icon={faHouse}/>  Home</div></div>
+               </NavLink> 
+               <NavLink className={style.linkMenu} >
+                  <div className={style.itemsDiv} ><div className={style.ocltar}><FontAwesomeIcon icon={faHeart} />  Favorites</div></div>
+               </NavLink>
+               <NavLink className={style.linkMenu} >
+                  <div className={style.itemsDiv} ><div className={style.ocltar}><FontAwesomeIcon icon={faCircleUser} />  About Me</div></div>
+               </NavLink>
+               <div  className={style.itemsDiv}><div className={style.ocltar}><FontAwesomeIcon icon={faRightFromBracket} />  Log Out</div></div>
+            </div>}
+         <div onClick={() => setMenuHamburguesa(!menuHamburguesa)}>
+            <FontAwesomeIcon icon={faBars} className={style.menuBurger}/>
+         </div>
+      </nav>
+
+      </>
+
+      
+   );
+}
+
+export default Nav;
---
+++ Server/src/index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const data = require('./utils/data')
+
+
+const PORT = 3001; 
+
+const server = http.createServer((req, res) => {
+    console.log(`Server raised in port ${PORT}`);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    const { url } = req;
+
+    switch (true) {
+        case url.includes("/rickandmorty/character/"):
+                const id = url.split('/').pop();
+                const personaje = data.find(element => element.id === parseInt(id));
+                res.writeHead(200, {'Content-Type' : 'application/json'});
+                return res.end(JSON.stringify(personaje));
+        
+        default:
+            res.writeHead(404, {'Content-Type' : 'text/plain'}) 
+            return res.end("Route not found");
+    }
+    
+}).listen(PORT, 'localhost')
+
+
+
+
